f68f5edc007307bd6a18ea99d589b8a572232bf2
InsAsam <108653442+InsAsam@users.noreply.github.com>
2024-05-24 02:41:03 +0200

    add something

--- src/ray_and_pixel.c
+++ src/ray_and_pixel.c
@@ -6,7 +6,7 @@
 /*   By: iassambe <iassambe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 18:54:23 by dkurcbar          #+#    #+#             */
-/*   Updated: 2024/05/20 20:03:29 by iassambe         ###   ########.fr       */
+/*   Updated: 2024/05/24 01:38:20 by iassambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ t_ray	make_ray(t_vector2 point)
 	t_ray	new_ray;
 
 	(void)point;
+	(void)new_ray;
+	new_ray.direction.x = 0;
 	return (new_ray);
 }
