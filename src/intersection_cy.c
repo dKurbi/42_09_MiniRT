3b5986865d848d53842bf6cc913a9a7311c111c4
dKurbi <dkurbi@gmail.com>
2024-07-15 17:07:23 +0200

    top and base of the cylinder drawing

--- src/intersection_cy.c
+++ src/intersection_cy.c
@@ -6,7 +6,7 @@
 /*   By: diego <diego@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 05:38:26 by iassambe          #+#    #+#             */
-/*   Updated: 2024/07/15 15:26:38 by diego            ###   ########.fr       */
+/*   Updated: 2024/07/15 16:53:04 by diego            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,72 @@ t_vector calculate_normal(t_cylinder cy, t_vector hit)
     t_vector proj_point = v_add(cy.cy_center, v_expand(cy.cy_axis, proj_length));
     return v_normalized(v_rest(hit, proj_point));
 }
+t_intersec inter_ray_base_cy(t_cylinder cy, t_ray ray, t_cy_inter_values val)
+{
+	float t_base;
+	t_intersec i_ret;
+	t_vector hit_base;
+	t_vector base_to_hit;
+	static	int i; // debug
+
+	i_ret.object = NO_INTER;
+    t_base = v_dot(v_rest(val.cylBase, ray.start), cy.cy_axis) / v_dot(ray.direction, cy.cy_axis);
+    hit_base = v_add(ray.start, v_expand(ray.direction, t_base));
+    base_to_hit = v_rest(hit_base, val.cylBase);
+    
+    if (v_dot(base_to_hit, base_to_hit) <= pow(cy.cy_diam / 2, 2) && t_base > 0) 
+	{
+        i_ret.object = CYLINDER;
+        i_ret.t1 = t_base;
+        i_ret.hit1 = hit_base;
+        i_ret.color = cy.cy_color;
+        i_ret.n1 = cy.cy_axis;
+		if (i++ == 0)
+			printf("----\n----\nBASE\n----\n----\n");
+			
+    }
+	return (i_ret);
+	
+}
+
+t_intersec inter_ray_top_cy(t_cylinder cy, t_ray ray, t_cy_inter_values val)
+{
+	t_intersec	i_ret;
+	t_vector	top_normal;
+	float		t_top;
+	t_vector	hit_top;
+	t_vector	top_to_hit;
+	static int 	i;
+
+	top_normal = v_expand(cy.cy_axis, -1);
+	i_ret.object = NO_INTER;
+	t_top = v_dot(v_rest(val.cylTop, ray.start), top_normal) / v_dot(ray.direction, top_normal);
+    hit_top = v_add(ray.start, v_expand(ray.direction, t_top));
+    top_to_hit = v_rest(hit_top, val.cylTop);
+    if (v_dot(top_to_hit, top_to_hit) <= pow(cy.cy_diam / 2, 2) && t_top > 0) 
+	{
+        i_ret.object = CYLINDER;
+        i_ret.t1 = t_top;
+        i_ret.hit1 = hit_top;
+        i_ret.color = cy.cy_color;
+        i_ret.n1 = top_normal;
+		if (i++ == 0)
+			printf("----\n----\nTOP\n----\n----\n");
+    }
+	return (i_ret);
+}
+// Intersección con la tapa y la base del cilindro
+t_intersec inter_ray_top_base_cy(t_cylinder cy, t_ray ray, t_cy_inter_values val)
+{
+
+	t_intersec i_ret;
+
+	i_ret = inter_ray_base_cy (cy, ray, val);
+	if (i_ret.object == CYLINDER)
+		return(i_ret);
+	i_ret = inter_ray_top_cy (cy, ray, val);
+	return (i_ret);
+}
 
 t_intersec inter_ray_cy(t_cylinder cy, t_ray ray) 
 {
@@ -66,7 +132,11 @@ t_intersec inter_ray_cy(t_cylinder cy, t_ray ray)
 		i_ret.hit1 = val.hit1;
 		i_ret.color = cy.cy_color;
         i_ret.n1 = calculate_normal(cy, i_ret.hit1);
+        return i_ret;
+
 	}
+	i_ret = inter_ray_top_base_cy(cy, ray, val);
+	
     return (i_ret);
 }
 
